27d07ec8b0aa1057eb718d18810d9a40dd6ba724
MitDune <mitchellsandberg9923@gmail.com>
2022-12-18 12:47:21 -0500

    Quick Update For Dash
    Added a condition for dash to prevent it from using stamina when speed is 0 and multiplying it would result in no movement

--- abilities/Dash/Dash.gd
+++ abilities/Dash/Dash.gd
@@ -3,7 +3,7 @@ extends Node3D
 @export var dash_speed : float = 40
 
 func execute(s, direction):
-	if Input.is_action_just_pressed("shift") and s.current_stamina >= 20:
+	if Input.is_action_just_pressed("shift") and s.current_stamina >= 20 and s.velocity != Vector3.ZERO:
 		s.velocity.x = direction.x * dash_speed
 		s.velocity.z = direction.z * dash_speed
 		s.current_stamina -= 20
