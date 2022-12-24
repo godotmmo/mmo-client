22e2c913b5e069bf6310a549dc03a7d1a115bbf2
MitDune <mitchellsandberg9923@gmail.com>
2022-12-24 18:37:23 -0500

    The Create Account and Password Hashing Update
    Added RPC calls and a new interface for creating an account on the server and logging in with it. Some password hashing was added on the server side so we added a first sha hash when passing the password through the first rpc calls for verifying and creating passwords.

--- gui/login/login.gd
+++ gui/login/login.gd
@@ -1,8 +1,19 @@
 extends Control
 
-@onready var username_input = get_node("NinePatchRect/HBoxContainer/VBoxContainer/Username")
-@onready var userpassword_input = get_node("NinePatchRect/HBoxContainer/VBoxContainer/Password")
-@onready var login_button = get_node("NinePatchRect/HBoxContainer/VBoxContainer/LoginButton")
+# UI state nodes
+@onready var login_screen = get_node("Login")
+@onready var create_account_screen = get_node("CreateAccount")
+# login nodes
+@onready var username_input = get_node("Login/HBoxContainer/VBoxContainer/Username")
+@onready var userpassword_input = get_node("Login/HBoxContainer/VBoxContainer/Password")
+@onready var login_button = get_node("Login/HBoxContainer/VBoxContainer/LoginButton")
+@onready var create_account_button = get_node("Login/HBoxContainer/VBoxContainer/CreateUsernameButton")
+# create new account nodes
+@onready var create_username_input = get_node("CreateAccount/HBoxContainer/VBoxContainer/Username")
+@onready var create_password_input = get_node("CreateAccount/HBoxContainer/VBoxContainer/Password2")
+@onready var create_confirm_password_input = get_node("CreateAccount/HBoxContainer/VBoxContainer/ConfirmPassword")
+@onready var confirm_button = get_node("CreateAccount/HBoxContainer/VBoxContainer/Confirm")
+@onready var back_button = get_node("CreateAccount/HBoxContainer/VBoxContainer/Back")
 
 func _on_LoginButton_pressed():
 	print("pressed")
@@ -14,7 +25,39 @@ func _on_login_button_button_down():
 		print("Please provide valid username and password")
 	else:
 		login_button.disabled = true
+		create_account_button.disabled = true
 		var username: String = username_input.get_text()
 		var password: String = userpassword_input.get_text()
 		print("Attempting to login")
-		Gateway.ConnectToServer(username, password)
+		Gateway.ConnectToServer(username, password, false)
+
+
+func _on_create_username_button_pressed():
+	login_screen.hide()
+	create_account_screen.show()
+
+
+func _on_back_pressed():
+	create_account_screen.hide()
+	login_screen.show()
+
+
+func _on_confirm_pressed():
+	
+	if create_username_input.get_text() == "":
+		print("Please provide a valid username")
+	elif create_password_input.get_text() == "":
+		print("Please provide a valid password")
+	elif create_confirm_password_input.get_text() == "":
+		print("Please confirm your password")
+	elif create_password_input.get_text() != create_confirm_password_input.get_text():
+		print("Passwords do not match")
+	elif create_password_input.get_text().length() <= 6:
+		print("Password must be at least 7 characters long")
+	else:
+		print("Create username request sent")
+		confirm_button.disabled = true
+		back_button.disabled = true
+		var username = create_username_input.get_text()
+		var password = create_password_input.get_text()
+		Gateway.ConnectToServer(username, password, true)
--- server/Gateway.gd
+++ server/Gateway.gd
@@ -8,6 +8,7 @@ var port: int = 44122
 
 var username: String
 var password: String
+var new_account: bool = false
 var peer_id
 
 var cert = load("res://resources/certification/X509_Certificate.crt")
@@ -25,9 +26,10 @@ func _process(_delta):
 		multiplayer.poll()
 
 	
-func ConnectToServer(_username, _password):
+func ConnectToServer(_username: String, _password: String, _new_account: bool):
 	username = _username
 	password = _password
+	new_account = _new_account
 	
 	# This defines our 'network' as a client peer
 	gateway_client.create_client(ip, port)
@@ -44,9 +46,13 @@ func ConnectToServer(_username, _password):
 	gateway_api.connected_to_server.connect(_OnConnectionSucceeded)
 	
 	
-func _OnConnectionFailed(_server_id):
+func _OnConnectionFailed():
 	print("Failed to connect to gateway server")
-	get_node("res://gui/login/login.tscn/NinePatchRect/HBoxContainer/VBoxContainer/LoginButton")
+	# re-enabled all button on the login screen if the request fails
+	get_node("/root/login_screen/Login/HBoxContainer/VBoxContainer/LoginButton").disabled = false
+	get_node("/root/login_screen/Login/HBoxContainer/VBoxContainer/CreateAccountButton").disabled = false
+	get_node("/root/login_screen/CreateAccount/HBoxContainer/VBoxContainer/Confirm").disabled = false
+	get_node("/root/login_screen/CreateAccount/HBoxContainer/VBoxContainer/Back").disabled = false
 	pass
 
 
@@ -56,7 +62,10 @@ func _OnConnectionDisconnected(_server_id):
 	
 func _OnConnectionSucceeded():
 	#print("Succesfully connected to gateway server id: %d" % server_id)
-	LoginRequest(username, password)
+	if new_account:
+		RequestCreateAccount()
+	else:
+		LoginRequest(username, password)
 
 
 @rpc(call_local)
@@ -64,7 +73,7 @@ func LoginRequest(_username, _password):
 	#var peer_id = gateway.get_multiplayer_peer().get_unique_id()
 	peer_id = 1
 	print("Requesting login user. uid: " + str(peer_id))
-	rpc_id(peer_id, "LoginRequest", _username, _password)
+	rpc_id(peer_id, "LoginRequest", _username, _password.sha256_text())
 	username = ""
 	password = ""
 
@@ -80,10 +89,42 @@ func ReturnLoginRequest(result, token):
 		# if invalid login, unlock the login button
 		print("Please provide a correct username and password")
 		print(get_node("."))
-		get_node("/root/login_screen/NinePatchRect/HBoxContainer/VBoxContainer/LoginButton").disabled = false
+		get_node("/root/login_screen/Login/HBoxContainer/VBoxContainer/LoginButton").disabled = false
+		get_node("/root/login_screen/Login/HBoxContainer/VBoxContainer/CreateUsernameButton").diabled = false
 	multiplayer.disconnect("connection_failed", _OnConnectionFailed)
 	gateway_client.disconnect("peer_disconnected", _OnConnectionDisconnected)
 	gateway_api.disconnect("connected_to_server", _OnConnectionSucceeded)
+
+
+@rpc(call_local)
+func RequestCreateAccount():
+	print("Requesting new account")
+	rpc_id(1, "CreateAccountRequest", username, password.sha256_text())
+	username = ""
+	password = ""
+	
+@rpc
+func CreateAccountRequest():
+	# Needed for rpc checksum
+	pass
+	
+@rpc(call_remote)
+func ReturnCreateAccountRequest(results, message):
+	print("Results from create account request recieved")
+	if results == true:
+		print("Account created")
+		get_node("/root/login_screen")._on_back_pressed()
+		get_node("/root/login_screen/CreateAccount/HBoxContainer/VBoxContainer/Back").disabled = false
+	else:
+		if message == 1:
+			print("Could not create account. Please try later.")
+		elif message == 2:
+			print("Username already exists")
+			get_node("/root/login_screen/CreateAccount/HBoxContainer/VBoxContainer/Confirm").disabled = false
+			get_node("/root/login_screen/CreateAccount/HBoxContainer/VBoxContainer/Back").disabled = false
+	gateway_api.connection_failed.disconnect(_OnConnectionFailed)
+	gateway_client.peer_disconnected.disconnect(_OnConnectionDisconnected)
+	gateway_api.connected_to_server.disconnect(_OnConnectionSucceeded)
 
 	
 	
